0b9dfc5ed52f5d0859ec6df1a0c3d77cb8eb3dda
jlb1999 <jlbroughton88@gmail.com>
2019-12-12 12:07:38 -0500

    added static route for build

--- server.js
+++ server.js
@@ -72,7 +72,13 @@ app.get("/", (req, res) => {
 //     connection.execSql(request)
 // }
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 app.listen( process.env.PORT || 5001 , () => {
     console.log("Server listening on port 5001");
