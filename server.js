1d465c009616424021ca7ee9c9e2a1c6da3de8f1
jlb1999 <jlbroughton88@gmail.com>
2019-12-12 10:49:33 -0500

    test

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const tediousExpress = require("express4-tedious")
 
 require("dotenv").config();
 
-const { Connection, Request } = require("tedious");
 
 const SQL_USER = process.env.SQL_USERNAME;
 const SQL_PASS = process.env.SQL_PASSWORD;
@@ -13,64 +11,66 @@ const SQL_SERVER = process.env.SQL_SERVER;
 console.log(SQL_SERVER);
 
 // Create connection to DB
-const config = {
-    authentication: {
-        options: {
-            userName: SQL_USER,
-            password: SQL_PASS
-        },
-        type: "default"
-    },
-    server: SQL_SERVER,
-    options: {
-        database: "BugTracker",
-        encrypt: true
-    }
-};
+// const config = {
+//     authentication: {
+//         options: {
+//             userName: SQL_USER,
+//             password: SQL_PASS
+//         },
+//         type: "default"
+//     },
+//     server: SQL_SERVER,
+//     options: {
+//         database: "BugTracker",
+//         encrypt: true
+//     }
+// };
 
-const connection = new Connection(config);
+// const connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through.
-connection.on("connect", err => {
-    if(err) {
-        console.error(err.message);
-    } else {
-        console.log("connection made!")
-        // queryDatabase();
-    }
-})
+// connection.on("connect", err => {
+//     if(err) {
+//         console.error(err.message);
+//     } else {
+//         console.log("connection made!")
+//         // queryDatabase();
+//     }
+// })
 
-app.use((req, res, next) => {
-    req.sql = tediousExpress(req, connection);
-    console.log(req.sql)
-    next();
-})
+// app.use((req, res, next) => {
+//     req.sql = tediousExpress(req, connection);
+//     console.log(req.sql)
+//     next();
+// })
 
-router.get("/", (req, res) => {
+// router.get("/", (req, res) => {
+//     res.send("Hello")
+// })
+
+app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-function queryDatabase() {
-    console.log("REading rows from the table...");
+// function queryDatabase() {
+//     console.log("REading rows from the table...");
 
-    // Read all rows from table
-    // Make a query
-    const request = new Request('SELECT * FROM bt_user', (err, rowCount) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(rowCount + " rows")
-        }
-    })
-    request.on('row', (columns) => {
-        columns.forEach((column) => {
-            console.log(column.value)
-        })
-    })
-    connection.execSql(request)
-
-
-}
+//     // Read all rows from table
+//     // Make a query
+//     const request = new Request('SELECT * FROM bt_user', (err, rowCount) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(rowCount + " rows")
+//         }
+//     })
+//     request.on('row', (columns) => {
+//         columns.forEach((column) => {
+//             console.log(column.value)
+//         })
+//     })
+//     connection.execSql(request)
+// }
 
 
 
